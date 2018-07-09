134e554b814ec907214f5609757b3210eb06da1b
Simon <simonren@jidouauto.com>
2018-07-09 12:03:13 +0800

    modify language

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,6 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-
     },
 
     // deviceready Event Handler
@@ -36,12 +35,9 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
-
     }
 
 
